0a1065a0428b7272e7f5f95b3ba041268558099c
DanielMA <danoan2008@gmail.com>
2024-09-22 11:51:51 +0200

    Search configuration file in the working directory

--- src/danoan/llm_assistant/core/api.py
+++ src/danoan/llm_assistant/core/api.py
@@ -3,14 +3,31 @@ from danoan.llm_assistant.core import exception, model
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 
+from functools import lru_cache
+import logging
 import os
 from pathlib import Path
+import sys
 import toml
 
-# -------------------- Configuration file --------------------
+logger = logging.getLogger(__file__)
+handler = logging.StreamHandler(sys.stderr)
+handler.setLevel(logging.DEBUG)
+handler.setFormatter(logging.Formatter("%(levelname)s: %(message)s"))
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
 
 LLM_ASSISTANT_ENV_VARIABLE = "LLM_ASSISTANT_CONFIGURATION_FOLDER"
 
+################################################################
+# Helper functions
+################################################################
+
+
+################################################################
+# Configuration file
+################################################################
 
 def get_configuration_folder() -> Path:
     if LLM_ASSISTANT_ENV_VARIABLE not in os.environ:
@@ -19,8 +36,39 @@ def get_configuration_folder() -> Path:
     return Path(os.environ[LLM_ASSISTANT_ENV_VARIABLE]).expanduser()
 
 
+@lru_cache
 def get_configuration_filepath() -> Path:
-    return get_configuration_folder() / "config.toml"
+    """
+    Return path to llm-assistant configuration file.
+
+    It traverses the parents of the working directory until
+    the configuration file is found.
+
+    If not found, checks if LLM_ASSISTANT_ENV_VARIABLE is 
+    defined and then use the configuration file located there.
+
+    Raises:
+        FileNotFoundError:                  if the configuration file is not found in 
+                                            the working directory.
+        EnvironmentVariableNotDefinedError: If the LLM_ASSISTANT_ENV_VARIABLE
+                                            is not defined
+    """
+    config_filename = "llm-assistant-config.toml"
+    visited = set()
+    folders_to_visit = [Path.cwd()]
+    while len(folders_to_visit) > 0:
+        cur_folder = folders_to_visit.pop()
+        if cur_folder in visited:
+            break
+        visited.add(cur_folder)
+        folders_to_visit.append(cur_folder.parent)
+        for p in cur_folder.iterdir():
+            if not p.is_dir():
+                if p.name == config_filename:
+                    return p
+                continue
+
+    return get_configuration_folder() / config_filename
 
 
 def get_configuration() -> model.LLMAssistantConfiguration:
