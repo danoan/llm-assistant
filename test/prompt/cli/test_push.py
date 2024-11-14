06ce367c370ba3e0c28af8102407919405069e9e
DanielMA <danoan2008@gmail.com>
2024-11-14 22:48:48 +0100

    Add tests

---
+++ test/prompt/cli/test_push.py
@@ -0,0 +1,30 @@
+"""
+Manual Test
+
+Initial State:
+    Current Branch: Master
+    Tags: No Tags
+
+Action 1:
+    Create a new file: banana
+    Call versioning push
+    Select Prompt Tweak
+    Create version 1.0.0
+    Create Branch: v1.0
+    Push changes
+
+Action 2:
+    Create a new file: apple
+    Call versioning push
+    Select Prompt Tweak
+    Create version 1.0.1
+    Keep within the same branch v1.0
+    Push changes
+
+Action 3:
+    Select Interface change
+
+Action 4:
+    Select Scope change
+
+"""
