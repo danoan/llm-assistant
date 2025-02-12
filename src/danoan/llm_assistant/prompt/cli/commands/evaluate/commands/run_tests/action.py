517a1bcec112494d5066f819ea0dcf3915452aca
Daniel Martins Antunes <danoan2008@gmail.com>
2025-02-12 11:14:39 +0100

    Allow run-tests to compare over two specific versions

--- src/danoan/llm_assistant/prompt/cli/commands/evaluate/commands/run_tests/action.py
+++ src/danoan/llm_assistant/prompt/cli/commands/evaluate/commands/run_tests/action.py
@@ -1,13 +1,16 @@
-from danoan.llm_assistant.common.model import RunnerConfiguration
+from danoan.llm_assistant.common.model import PromptConfiguration, RunnerConfiguration
 
 from danoan.llm_assistant.common import config
 from danoan.llm_assistant.prompt.cli import utils as cli_utils
 from danoan.llm_assistant.prompt.core import api
 from danoan.llm_assistant.runner.core import api as llma
 
+import git
 import json
 import logging
 import sys
+import toml
+from tempfile import TemporaryDirectory
 
 logger = logging.getLogger(__file__)
 handler = logging.StreamHandler(sys.stderr)
@@ -17,44 +20,80 @@ logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
 
-def __run_tests__(runner_configuration: RunnerConfiguration, prompt_name: str):
-    llma.LLMAssistant().setup(runner_configuration)
-    prompt_config = config.get_prompt_configuration(prompt_name)
+def __load_prompt_configuration_version__(
+    prompt_name: str, version: str
+) -> PromptConfiguration:
+    tp = api.get_tracked_prompt(prompt_name)
+    repo = git.Repo(tp.repository_path)
+
+    if version == "current":
+        config_toml = repo.git.show(f"HEAD:config.toml")
+    else:
+        repo.git.checkout(version)
+        config_toml = repo.git.show(f"{version}:config.toml")
+
+    return PromptConfiguration(**toml.loads(config_toml))
+
+
+def __run_tests__(
+    runner_configuration: RunnerConfiguration,
+    prompt_name: str,
+    base_version: str,
+    compare_version: str,
+):
+    base_prompt_config = __load_prompt_configuration_version__(
+        prompt_name, base_version
+    )
+    compare_prompt_config = __load_prompt_configuration_version__(
+        prompt_name, compare_version
+    )
 
     test_model = api.get_prompt_test_regression_filepath(prompt_name)
     test_run = test_model.parent / runner_configuration.model / test_model.name
-    if not test_run.exists():
-        logger.error(
-            f"No test run file for prompt {prompt_config.name} and model {runner_configuration.model} was generated yet. You need to generate one first using the rengerate-tests command."
-        )
-        exit(1)
 
     with open(test_run, "r") as fi:
         input_obj = json.load(fi)
 
         for entry in input_obj:
-            response = llma.custom(prompt_config, **entry["input"])
+            base_response = llma.custom(base_prompt_config, **entry["input"])
+            compare_response = llma.custom(compare_prompt_config, **entry["input"])
+
             try:
-                response_obj = json.loads(response.content)
+                base_response_obj = json.loads(base_response.content)
             except:
-                response_obj = response.content
+                base_response_obj = base_response.content
 
-            response_str = json.dumps(response_obj, ensure_ascii=False, indent=2)
-            expected_str = json.dumps(entry["output"], ensure_ascii=False, indent=2)
+            try:
+                compare_response_obj = json.loads(compare_response.content)
+            except:
+                compare_response_obj = compare_response.content
 
-            yield (response_str, expected_str)
+            base_response_str = json.dumps(
+                base_response_obj, ensure_ascii=False, indent=2
+            )
+            compare_response_str = json.dumps(
+                compare_response_obj, ensure_ascii=False, indent=2
+            )
+
+            yield (base_response_str, compare_response_str)
 
 
-def run_tests(prompt_name: str):
+def run_tests(
+    prompt_name: str, use_cache: bool, base_version: str, compare_version: str
+):
+    runner_configuration = config.get_configuration().runner
+    runner_configuration.use_cache = use_cache
+    llma.LLMAssistant().setup(runner_configuration)
+
     try:
-        for response_str, expected_str in __run_tests__(
-            config.get_configuration().runner, prompt_name
+        for base_response_str, compare_response_str in __run_tests__(
+            runner_configuration, prompt_name, base_version, compare_version
         ):
             cli_utils.print_side_by_side(
-                response_str,
-                expected_str,
-                left_title="New response",
-                right_title="Previous response",
+                base_response_str,
+                compare_response_str,
+                left_title="Base",
+                right_title="Compare",
             )
     except FileNotFoundError as ex:
         print(ex)
